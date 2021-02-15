bb40d44540564a0b6cfe1f4839f893c69753c0a9
tonystark007 <khanminnat31@gmail.com>
2021-02-15 20:47:43 +0530

    1038. Binary Search Tree to Greater Sum Tree

---
+++ binarySearchTreeToGreaterSumTree.cpp
@@ -0,0 +1,40 @@
+// Source : https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+// Date   : 11-02-2021
+
+/********************************************************************************** 
+
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.         
+
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+**********************************************************************************/
+
+class Solution {
+private:
+    int reverseInorder(TreeNode* root, int sum) {
+        if(!root)
+            return sum;
+        
+        int rightSum = reverseInorder(root->right, sum);
+        int currentSum = root->val + rightSum;
+        root->val = currentSum;
+        
+        int leftSum = reverseInorder(root->left, currentSum);
+        
+        return leftSum;
+    }
+    
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        reverseInorder(root, 0);
+        
+        return root;
+    }
+};
