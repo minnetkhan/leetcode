85825f029df91993e3d1e48052d655ef5c110976
tonystark007 <khanminnat31@gmail.com>
2021-03-04 17:21:46 +0530

    1038. Binary Search Tree to Greater Sum Tree

--- Binary-Search-Tree-To-Greater-Sum-Tree.cpp
+++ Binary-Search-Tree-To-Greater-Sum-Tree.cpp
@@ -10,7 +10,7 @@ As a reminder, a binary search tree is a tree that satisfies these constraints:
 
 The left subtree of a node contains only nodes with keys less than the node's key.
 The right subtree of a node contains only nodes with keys greater than the node's key.
-Both the left and right subtrees must also be binary search trees.         
+Both the left and right subtrees must also be binary search trees.      
 
 Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
 Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
