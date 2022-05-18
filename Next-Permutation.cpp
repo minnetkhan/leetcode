3e1c5e6e21f633787af774d78ec64b89951db0f8
minnetkhan <khanminnat31@gmail.com>
2022-05-18 16:28:41 +0530

    31. Next Permutation

---
+++ Next-Permutation.cpp
@@ -0,0 +1,39 @@
+// Source : https://leetcode.com/problems/next-permutation/
+// Date   : 18-05-2022
+
+/********************************************************************************** 
+
+A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are considered permutations of arr: [1,2,3], [1,3,2], [3,1,2], [2,3,1].
+
+Intuition:
+1. Find the largest index k such that nums[k] < nums[k + 1]. If no such index exists, just reverse nums and done.
+2. Find the largest index l > k such that nums[k] < nums[l].
+3. Swap nums[k] and nums[l].
+4. Reverse the sub-array nums[k + 1:].
+
+**********************************************************************************/
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size(), k, l;
+        for (k = n - 2; k >= 0; k--) {
+            if (nums[k] < nums[k + 1]) {
+                break;
+            }
+        }
+        if (k < 0) {
+            reverse(nums.begin(), nums.end());
+        } else {
+            for (l = n - 1; l > k; l--) {
+                if (nums[l] > nums[k]) {
+                    break;
+                }
+            } 
+            swap(nums[k], nums[l]);
+            reverse(nums.begin() + k + 1, nums.end());
+        }
+    }
+}; 
